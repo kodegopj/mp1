1b93064b2dd8f487f72f000a84595ce4d158336f
kodegopj <pauljohncunanan5@gmail.com>
2023-09-21 14:26:10 +0800

    initial

--- name.js
+++
@@ -1,8 +0,0 @@
-function toggleHiddenParagraph() {
-    var hiddenParagraph = document.getElementById('hidden-paragraph');
-    if (hiddenParagraph.style.display === 'none' || hiddenParagraph.style.display === '') {
-        hiddenParagraph.style.display = 'block';
-    } else {
-        hiddenParagraph.style.display = 'none';
-    }
-}
